47af34edffbcfb5afadef8e2f94bfe55cfcd68b4
Rudy Nurafif <rudynurafif@gmail.com>
2024-07-27 19:35:28 +0700

    App bar / Top bar

--- src/app/page.js
+++ src/app/page.js
@@ -1,19 +1,5 @@
 'use client';
 
-import { ColorModeContext, useMode } from '@/theme';
-import { CssBaseline, ThemeProvider } from '@mui/material';
-
 export default function Home() {
-  const [theme, colorMode] = useMode();
-
-  return (
-    <ColorModeContext.Provider value={colorMode}>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <div className=''>
-          <main className='content'></main>
-        </div>
-      </ThemeProvider>
-    </ColorModeContext.Provider>
-  );
+  return <div></div>;
 }
